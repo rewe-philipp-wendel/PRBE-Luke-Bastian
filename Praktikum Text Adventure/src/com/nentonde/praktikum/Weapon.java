a0455deb15b46a91a6d33b15517133e0ddb69d89
Nentonde <nentonde@gmail.com>
2022-01-19 11:42:38 +0100

    first commit

---
+++ Praktikum Text Adventure/src/com/nentonde/praktikum/Weapon.java
@@ -0,0 +1,71 @@
+package com.nentonde.praktikum;
+
+public class Weapon {
+
+    String weaponName;
+    double weaponCombatDamage;
+    double weaponCritChance;
+    double weaponMagicDamage;
+    int weaponMagicUsage;
+
+    //constructor
+    public Weapon(String weaponName, double weaponCombatDamage, double weaponCritChance, double weaponMagicDamage, int weaponMagicUsage) {
+        this.weaponName = weaponName;
+        this.weaponCombatDamage = weaponCombatDamage;
+        this.weaponCritChance = weaponCritChance;
+        this.weaponMagicDamage = weaponMagicDamage;
+        this.weaponMagicUsage = weaponMagicUsage;
+    }
+
+    //getter and setter
+    public String getWeaponName() {
+        return weaponName;
+    }
+
+    public void setWeaponName(String weaponName) {
+        this.weaponName = weaponName;
+    }
+
+    public double getWeaponCombatDamage() {
+        return weaponCombatDamage;
+    }
+
+    public void setWeaponCombatDamage(double weaponCombatDamage) {
+        this.weaponCombatDamage = weaponCombatDamage;
+    }
+
+    public double getWeaponCritChance() {
+        return weaponCritChance;
+    }
+
+    public void setWeaponCritChance(double weaponCritChance) {
+        this.weaponCritChance = weaponCritChance;
+    }
+
+    public double getWeaponMagicDamage() {
+        return weaponMagicDamage;
+    }
+
+    public void setWeaponMagicDamage(double weaponMagicDamage) {
+        this.weaponMagicDamage = weaponMagicDamage;
+    }
+
+    public int getWeaponMagicUsage() {
+        return weaponMagicUsage;
+    }
+
+    public void setWeaponMagicUsage(int weaponMagicUsage) {
+        this.weaponMagicUsage = weaponMagicUsage;
+    }
+
+    @Override
+    public String toString() {
+        return "Weapon{" +
+                "weaponName='" + weaponName + '\'' +
+                ", weaponCombatDamage=" + weaponCombatDamage +
+                ", weaponCritChance=" + weaponCritChance +
+                ", weaponMagicDamage=" + weaponMagicDamage +
+                ", weaponMagicUsage=" + weaponMagicUsage +
+                '}';
+    }
+}
