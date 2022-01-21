8653e9e13ff41dd5b075f920579bad1b784d2b26
Nentonde <nentonde@gmail.com>
2022-01-21 09:59:03 +0100

    Added getter, setter and a toString to the enemy class

--- src/com/nentonde/praktikum/Enemy.java
+++ src/com/nentonde/praktikum/Enemy.java
@@ -16,4 +16,57 @@ public class Enemy extends Person{
         this.enemyCP = enemyCP;
         this.enemyCritChance = enemyCritChance;
     }
+
+    public String getEnemyClass() {
+        return enemyClass;
+    }
+
+    public void setEnemyClass(String enemyClass) {
+        this.enemyClass = enemyClass;
+    }
+
+    public int getEnemyLevel() {
+        return enemyLevel;
+    }
+
+    public void setEnemyLevel(int enemyLevel) {
+        this.enemyLevel = enemyLevel;
+    }
+
+    public int getEnemyHP() {
+        return enemyHP;
+    }
+
+    public void setEnemyHP(int enemyHP) {
+        this.enemyHP = enemyHP;
+    }
+
+    public double getEnemyCP() {
+        return enemyCP;
+    }
+
+    public void setEnemyCP(double enemyCP) {
+        this.enemyCP = enemyCP;
+    }
+
+    public double getEnemyCritChance() {
+        return enemyCritChance;
+    }
+
+    public void setEnemyCritChance(double enemyCritChance) {
+        this.enemyCritChance = enemyCritChance;
+    }
+
+    @Override
+    public String toString() {
+        return "Enemy{" +
+                "enemyClass='" + enemyClass + '\'' +
+                ", enemyLevel=" + enemyLevel +
+                ", enemyHP=" + enemyHP +
+                ", enemyCP=" + enemyCP +
+                ", enemyCritChance=" + enemyCritChance +
+                ", personName='" + personName + '\'' +
+                ", personRace='" + personRace + '\'' +
+                '}';
+    }
 }
