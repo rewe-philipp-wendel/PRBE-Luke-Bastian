82deedb9016de9c8d1c14691ccafc53be2f85bf1
Nentonde <nentonde@gmail.com>
2022-01-21 09:59:48 +0100

    Worked on the gettingAttacked method

--- src/com/nentonde/praktikum/Main.java
+++ src/com/nentonde/praktikum/Main.java
@@ -1,12 +1,17 @@
 package com.nentonde.praktikum;
 
+import com.nentonde.praktikum.FightActions;
+
 public class Main {
 
-    Hero hero1 = new Hero("Link","Human","Knight", 6, 1, 20, 0.01, 1);
-    Weapon swordTraining = new Weapon("Trainings Schwert", 3, 0.00, 0, 0);
+
+    static Hero hero1 = new Hero("Link","Human","Knight", 6, 1, 20, 0.01, 1);
+    static Enemy enemy1 = new Enemy("Clark", "Human", "Archer", 1, 6, 1, 0.0);
+    static Weapon swordTraining = new Weapon("Trainings Schwert", 3, 0.00, 0, 0);
 
 
     public static void main(String[] args) {
-
+        hero1.setHeroHP(hero1.getHeroBaseHP());
+        FightActions.gettingAttacked(enemy1, enemy1.getEnemyCP(), hero1);
     }
 }
