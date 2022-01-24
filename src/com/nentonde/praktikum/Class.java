21f4ed10ab5262636a438d7e61c983d5bcda74f9
Nentonde <nentonde@gmail.com>
2022-01-24 14:26:28 +0100

    Tested some things with the Class class

--- src/com/nentonde/praktikum/Class.java
+++ src/com/nentonde/praktikum/Class.java
@@ -1,10 +1,33 @@
 package com.nentonde.praktikum;
 
+import java.util.Scanner;
+
 public class Class {
 
     String classClassName;
+    String classClassAction;
 
+    //contructor
     public Class(String classClassName) {
         this.classClassName = classClassName;
+    }
+
+    //getter and setter
+    public String getClassClassName() {
+        return classClassName;
+    }
+    public void setClassClassName(String classClassName) {
+        this.classClassName = classClassName;
+    }
+    public String getClassClassAction() {
+        return classClassAction;
+    }
+    public void setClassClassAction(String classClassAction) {
+        this.classClassAction = classClassAction;
+    }
+
+    //methods
+    public static void useAction(){
+        Scanner scanner = new Scanner(System.in);
     }
 }
--- src/com/nentonde/praktikum/Main.java
+++ src/com/nentonde/praktikum/Main.java
@@ -1,9 +1,11 @@
 package com.nentonde.praktikum;
 
 import com.nentonde.praktikum.FightActions;
+import com.nentonde.praktikum.Class;
 
 public class Main {
 
+    //objects
     static Class class1 = new Class("Klasse 1");
     static Hero hero1 = new Hero("Link","Human", class1, 6, 1, 20, 0.01, 1);
     static Enemy enemy1 = new Enemy("Clark", "Human", "Archer", 1, 6, 1, 0.0);
@@ -13,5 +15,6 @@ public class Main {
     public static void main(String[] args) {
         //hero1.setHeroHP(hero1.getHeroBaseHP());
         FightActions.gettingAttacked(enemy1, enemy1.getEnemyCP(), hero1);
+        Class.useAction();
     }
 }
